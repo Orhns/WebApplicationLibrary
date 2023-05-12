18e27c0a96a3b7b08a3afb1d3612f9c749d723f7
Orhun <78608829+Orhns@users.noreply.github.com>
2023-05-12 15:41:17 +0300

    author management

--- a_author_mng.aspx
+++ a_author_mng.aspx
@@ -1,10 +1,20 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/masterpage.Master" AutoEventWireup="true" CodeBehind="a_author_mng.aspx.cs" Inherits="WebApplicationLibrary.a_author_mng" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <script type="text/javascript">
+      $(document).ready(function () {
+      
+          //$(document).ready(function () {
+              //$('.table').DataTable();
+          // });
+          $(".table").prepend($("<thead></thead>").append($(this).find("tr:first"))).dataTable();
+          //$('#GridView1').DataTable();
+      });
+    </script>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-
-    <div class="container">
-        <div class="row">
+    <div style="background-color:cadetblue">
+    <div class="container pt-4">
+        <div class="row ">
             <!-- left column -->
             <div class="col-md-5">
                 <div class="card">
@@ -29,15 +39,15 @@
                                 <label for="TextBox1" class="form-label">Author ID</label>
                                 <div class="form-group">
                                     <div class="input-group">
-                                        <asp:TextBox CssClass="form-control" ID="TextBox1" runat="server" placeholder="ID"></asp:TextBox>
-                                        <asp:Button class="btn btn-primary" ID="Button2" runat="server" Text="Get" />
+                                        <asp:TextBox CssClass="form-control" ID="authorid" runat="server" placeholder="ID"></asp:TextBox>
+                                        <asp:Button class="btn btn-primary" ID="getButton" runat="server" Text="Get" OnClick="getButton_Click"  />
                                     </div>
                                 </div>
                             </div>
                             <div class="col-md-8">
                                 <label for="TextBox2" class="form-label">Author Name</label>
                                 <div class="form-group">
-                                    <asp:TextBox CssClass="form-control" ID="TextBox2" runat="server" placeholder="Author Name"></asp:TextBox>
+                                    <asp:TextBox CssClass="form-control" ID="authorName" runat="server" placeholder="Author Name"></asp:TextBox>
                                 </div>
                             </div>
                         </div>
@@ -45,17 +55,17 @@
                         <div class="row mt-3">
                             <div class="col-4">
                                 <div class="d-grid gap-2">
-                                    <asp:Button class="btn btn-success" ID="Button1" runat="server" Text="Add" />
+                                    <asp:Button class="btn btn-success" ID="addButton" runat="server" Text="Add" OnClick="addButton_Click"  />
                                 </div>
                             </div>
                             <div class="col-4">
                                 <div class="d-grid gap-2">
-                                    <asp:Button class="btn btn-warning" ID="Button3" runat="server" Text="Update" />
+                                    <asp:Button class="btn btn-warning" ID="updateButton" runat="server" Text="Update" OnClick="updateButton_Click" />
                                 </div>
                             </div>
                             <div class="col-4">
                                 <div class="d-grid gap-2">
-                                    <asp:Button class="btn btn-danger" ID="Button4" runat="server" Text="Delete" />
+                                    <asp:Button class="btn btn-danger" ID="deleteButton" runat="server" Text="Delete" OnClick="deleteButton_Click" />
                                 </div>
                             </div>
                         </div>
@@ -82,8 +92,14 @@
                             </div>
                         </div>
                         <div class="row">
+                            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" ProviderName="System.Data.SqlClient" SelectCommand="SELECT * FROM [author_master_tbl]"></asp:SqlDataSource>
                             <div class="col">
-                                <asp:GridView class="table" ID="GridView1" runat="server"></asp:GridView>
+                                <asp:GridView class="table table-striped table-bordered table-hover"  ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="author_id" DataSourceID="SqlDataSource1">
+                                    <Columns>
+                                        <asp:BoundField DataField="author_id" HeaderText="author_id" ReadOnly="True" SortExpression="author_id" />
+                                        <asp:BoundField DataField="author_name" HeaderText="author_name" SortExpression="author_name" />
+                                    </Columns>
+                                </asp:GridView>
                             </div>
                         </div>
                     </div>
@@ -93,5 +109,5 @@
             </div>
         </div>
     </div>
-
+    </div>
 </asp:Content>
--- a_author_mng.aspx.cs
+++ a_author_mng.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +12,189 @@ namespace WebApplicationLibrary
 {
     public partial class a_author_mng : System.Web.UI.Page
     {
+        string conn = ConfigurationManager.ConnectionStrings["connectionstr"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            if (chechAutID())
+            {
+                Response.Write("<script>alert('Author ID is already taken')</script>");
+            }
+            else
+            {
+                addNewAuth();
+            }
+            clearForm();
+            GridView1.DataBind();
+        }
+
+        protected void updateButton_Click(object sender, EventArgs e)
+        {
+            if (chechAutID() == false)
+            {
+                Response.Write("<script>alert('Please enter correct Author ID')</script>");
+            }
+            else
+            {
+                updateAuth();
+            }
+            clearForm();
+            GridView1.DataBind();
+        }
+
+        protected void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (chechAutID() == false)
+            {
+                Response.Write("<script>alert('Please enter correct Author ID')</script>");
+            }
+            else
+            {
+                deleteAuth();
+            }
+            clearForm();
+            GridView1.DataBind();
+        }
+
+        protected void getButton_Click(object sender, EventArgs e)
+        {
+            getAuth();
+        }
+        bool checkidhelper;
+        bool chechAutID()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(conn);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("SELECT * FROM author_master_tbl WHERE author_id = @auID;",con);
+                cmd.Parameters.AddWithValue("@auID",authorid.Text.Trim());
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    checkidhelper = true;
+                }
+                else
+                {
+                    checkidhelper = false;
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+            return checkidhelper;
+        }
+
+        void addNewAuth()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(conn);
+                if(con.State == ConnectionState.Closed)
+                {
+                    con.Open(); 
+                }
+                SqlCommand cmd = new SqlCommand("INSERT INTO author_master_tbl (author_id,author_name) VALUES (@auID,@auName)", con);
+                cmd.Parameters.AddWithValue("@auID", authorid.Text.Trim());
+                cmd.Parameters.AddWithValue("@auName", authorName.Text.Trim());
+                cmd.ExecuteNonQuery();
+                con.Close() ;
+                Response.Write("<script>alert('Author added succesfully.')</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        void updateAuth()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(conn);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("UPDATE author_master_tbl SET author_name = @auName WHERE author_id = @auID;", con);
+                cmd.Parameters.AddWithValue("@auID", authorid.Text.Trim());
+                cmd.Parameters.AddWithValue("@auName", authorName.Text.Trim());
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Author name updated succesfully.')</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        void deleteAuth()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(conn);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("DELETE FROM author_master_tbl WHERE author_id = @auID;", con);
+                cmd.Parameters.AddWithValue("@auID", authorid.Text.Trim());
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Author deleted succesfully.')</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        void getAuth()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(conn);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("SELECT * FROM author_master_tbl WHERE author_id = @auID;", con);
+                cmd.Parameters.AddWithValue("@auID", authorid.Text.Trim());
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        authorName.Text = reader.GetString(1);
+                    }
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid author ID')</script>");
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+            
+        }
+
+        void clearForm()
+        {
+            authorid.Text = null;
+            authorName.Text = null;
         }
     }
 }
